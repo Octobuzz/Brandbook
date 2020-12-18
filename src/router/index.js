418626a3156fbe3cb6a4ef6a0933015123506119
Ольга Пастухова <olg.pastukhova1@x5.ru>
2020-12-18 06:16:43 +0300

    Созданы основные компоненты и страницы

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Identics from '../views/Identics.vue';
+import Process from '../views/Process.vue';
+import Materials from '../views/Materials.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'identics',
+    component: Identics,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/process',
+    name: 'process',
+    component: Process,
+  },
+  {
+    path: '/materials',
+    name: 'material',
+    component: Materials,
   },
 ];
 
