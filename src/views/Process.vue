8206c958f797682e14976cef0485b7eff3d583b8
Ольга Пастухова <olg.pastukhova1@x5.ru>
2020-12-26 04:14:58 +0300

    Добавлены картинки для 4 стадий процесса

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -20,47 +20,75 @@
 				<h3 class="process-block__title">
 					Отрицание
 				</h3>
-				<ul class="process-block__list">
-					<li><b>Шаг 1.</b>  Обсуждаем первичные ассоциации с клевером, технологиями</li>
-					<li><b>Шаг 2.</b> Получаем логотипы перекрестка, Sims, Windows и свастики различных оттенков зеленого</li>
-					<li><b>Шаг 3.</b> Изучаем логотипы IT-компаний, логотипы с клевером</li>
-					<li><b>Шаг 4.</b> Понимаем, что все, что мы сделали, было уже сделано до нас. Печально</li>
-					<li><b>Вывод:</b> Осознанно отказываемся от классических образов клевера, прямых ассоциаций с технологиями и зеленого цвета как доминирующего</li>
-				</ul>
+				<div class="process-block__content">
+					<img
+						src="../img/denial.jpeg"
+						alt="Отрицание"
+						class="process-block__img"
+					/>
+					<ul class="process-block__list">
+						<li><b>Шаг 1.</b>  Обсуждаем первичные ассоциации с клевером, технологиями</li>
+						<li><b>Шаг 2.</b> Получаем логотипы перекрестка, Sims, Windows и свастики различных оттенков зеленого</li>
+						<li><b>Шаг 3.</b> Изучаем логотипы IT-компаний, логотипы с клевером</li>
+						<li><b>Шаг 4.</b> Понимаем, что все, что мы сделали, было уже сделано до нас. Печально</li>
+						<li><b>Вывод:</b> Осознанно отказываемся от классических образов клевера, прямых ассоциаций с технологиями и зеленого цвета как доминирующего</li>
+					</ul>
+				</div>
 			</div>
 			<div class="process-block">
 				<h3 class="process-block__title">
 					Гнев
 				</h3>
-				<ul class="process-block__list">
-					<li><b>Шаг 1.</b> Пытаемся спрятать образ клевер изящнее, обыграть тех</li>
-					<li><b>Шаг 2.</b> Понимаем, что название компании может изменится</li>
-					<li><b>Шаг 3.</b> Гневаемся</li>
-					<li><b>Шаг 4.</b> Забываем про шаг 2. Замечаем интересную деталь — в латинской букве "K" читается и "T". Думаем о монограмме.</li>
-					<li><b>Шаг 5.</b> Понимаем, что читается также и "IT", что делает деталь еще интереснее</li>
-					<li><b>Шаг 6.</b> Вспоминаем о шаге 2. Гневаемся</li>
-					<li><b>Вывод:</b> Оставляем идею с монограммой как потенциально удачную на будущее. Ищем образ универсальнее</li>
-				</ul>
+				<div class="process-block__content">
+					<img
+						src="../img/anger.png"
+						alt="Гнев"
+						class="process-block__img process-block__img--anger"
+					/>
+					<ul class="process-block__list">
+						<li><b>Шаг 1.</b> Пытаемся спрятать образ клевер изящнее, обыграть технологии</li>
+						<li><b>Шаг 2.</b> Понимаем, что название компании может изменится</li>
+						<li><b>Шаг 3.</b> Гневаемся (при должном умении можно тихохонько ругнуться)</li>
+						<li><b>Шаг 4.</b> Забываем про шаг 2. Замечаем интересную деталь — в латинской букве "K" читается и "T". Думаем о монограмме.</li>
+						<li><b>Шаг 5.</b> Понимаем, что читается также и "IT", что делает деталь еще интереснее</li>
+						<li><b>Шаг 6.</b> Вспоминаем о шаге 2. Гневаемся</li>
+						<li><b>Вывод:</b> Оставляем идею с монограммой как потенциально удачную на будущее. Ищем образ универсальнее</li>
+					</ul>
+				</div>
 			</div>
 			<div class="process-block">
 				<h3 class="process-block__title">
 					Торг 
 				</h3>
-				<ul class="process-block__list">
-					<li><b>Шаг 1.</b> За бог знает какие деньги и услуги просим знакомых нарисовать по одному логотипу</li>
-					<li><b>Шаг 2.</b> Снова получаем свастики</li>
-					<li><b>Шаг 3.</b> Уверенно идем в стадию 4</li>
-					<li><b>Вывод:</b> Крепко задумываемся об идеологических убеждениях друзей. Возвращаемся к ноше графического дизайнера в одиночку</li>
-				</ul>
+				<div class="process-block__content">
+					<img
+						src="../img/trade.png"
+						alt="Торг"
+						class="process-block__img"
+					/>
+					<ul class="process-block__list process-block__list--trade">
+						<li><b>Шаг 1.</b> За бог знает какие деньги и услуги просим знакомых нарисовать по одному логотипу</li>
+						<li><b>Шаг 2.</b> Снова получаем свастики</li>
+						<li><b>Шаг 3.</b> Уверенно идем в стадию 4</li>
+						<li><b>Вывод:</b> Крепко задумываемся об идеологических убеждениях друзей. Возвращаемся к ноше графического дизайнера в одиночку</li>
+					</ul>
+				</div>
 			</div>
 			<div class="process-block">
 				<h3 class="process-block__title">
 					Депрессия 
 				</h3>
-				<ul class="process-block__list">
-					<li><b>Шаг 1.</b> Включаем плейлист саундтреков к Max Payne 2, грустно смотрим вдаль</li>
-					<li><b>Вывод:</b> Хорошая игра</li>
-				</ul>
+				<div class="process-block__content">
+					<img
+						src="../img/depression.jpg"
+						alt="Депрессия "
+						class="process-block__img"
+					/>
+					<ul class="process-block__list process-block__list--depression">
+						<li><b>Шаг 1.</b> Включаем плейлист саундтреков к Max Payne 2, грустно смотрим вдаль</li>
+						<li><b>Вывод:</b> Хорошая игра</li>
+					</ul>
+				</div>
 			</div>
 			<div class="process-block">
 				<h3 class="process-block__title">
@@ -98,7 +126,7 @@ export default class Process extends Vue {}
 	&-headline {
 		font-size: 28px;
 		font-weight: 400;
-		margin-top: 50px;
+		margin: 50px 0;
 	}
 
 	&-actions {
@@ -107,16 +135,32 @@ export default class Process extends Vue {}
 		justify-content: space-around;
 		position: absolute;
 		left: 50%;
-		top: 100px;
+		top: 130px;
 		transform: translateX(-50%);
 
 		&__button {
 			font: inherit;
 			background-color: transparent;
 			border: none;
+			position: relative;
 
 			&:hover {
 				cursor: pointer;
+				color: $main;
+			}
+
+			&:first-child::before {
+				content: url('../svg/chevron-left.svg');
+				position: absolute;
+				top: 5px;
+				left: -20px;
+			}
+
+			&:last-child::after {
+				content: url('../svg/chevron-right.svg');
+				position: absolute;
+				top: 6px;
+				right: -20px;
 			}
 		}
 	}
@@ -126,6 +170,7 @@ export default class Process extends Vue {}
 			display: inline-block;
 			font-size: 24px;
 			position: relative;
+			margin-bottom: 80px;
 
 			&::before {
 				content: '';
@@ -149,10 +194,41 @@ export default class Process extends Vue {}
 		}
 
 		&__list {
+			padding: 15px 0 15px 40px;
+			width: 670px;
 			list-style: none;
 			text-align: left;
+			background-color: $white;
+
+			&--depression {
+				margin-top: -15px;
+			}
+
+			&--trade {
+				padding: 42px 0 42px 40px;
+			}
+
 			& li {
 				margin-bottom: 7px;
+				&:last-child {
+					margin-bottom: 0;
+				}
+			}
+		}
+
+		&__content {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		&__img { 
+			width: 400px;
+			height: auto;
+			margin-right: -85px;
+
+			&--anger {
+				width: 415px;
 			}
 		}
 	}
