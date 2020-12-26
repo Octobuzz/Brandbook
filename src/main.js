51ebf352069412e6c5aefface9d08905613b1506
Ольга Пастухова <olg.pastukhova1@x5.ru>
2020-12-27 00:01:18 +0300

    Установлен свайпер

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 Vue.config.productionTip = false;
+Vue.use(VueAwesomeSwiper);
 
 new Vue({
-  router,
-  render: (h) => h(App),
+	router,
+	render: (h) => h(App),
 }).$mount('#app');
--- src/views/Process.vue
+++ src/views/Process.vue
@@ -16,85 +16,101 @@
 					Далее
 				</button>
 			</div>
-			<div class="process-block">
-				<h3 class="process-block__title">
-					Отрицание
-				</h3>
-				<div class="process-block__content">
-					<img
-						src="../img/denial.jpeg"
-						alt="Отрицание"
-						class="process-block__img"
-					/>
-					<ul class="process-block__list">
-						<li><b>Шаг 1.</b>  Обсуждаем первичные ассоциации с клевером, технологиями</li>
-						<li><b>Шаг 2.</b> Получаем логотипы перекрестка, Sims, Windows и свастики различных оттенков зеленого</li>
-						<li><b>Шаг 3.</b> Изучаем логотипы IT-компаний, логотипы с клевером</li>
-						<li><b>Шаг 4.</b> Понимаем, что все, что мы сделали, было уже сделано до нас. Печально</li>
-						<li><b>Вывод:</b> Осознанно отказываемся от классических образов клевера, прямых ассоциаций с технологиями и зеленого цвета как доминирующего</li>
-					</ul>
-				</div>
-			</div>
-			<div class="process-block">
-				<h3 class="process-block__title">
-					Гнев
-				</h3>
-				<div class="process-block__content">
-					<img
-						src="../img/anger.png"
-						alt="Гнев"
-						class="process-block__img process-block__img--anger"
-					/>
-					<ul class="process-block__list">
-						<li><b>Шаг 1.</b> Пытаемся спрятать образ клевер изящнее, обыграть технологии</li>
-						<li><b>Шаг 2.</b> Понимаем, что название компании может изменится</li>
-						<li><b>Шаг 3.</b> Гневаемся (при должном умении можно тихохонько ругнуться)</li>
-						<li><b>Шаг 4.</b> Забываем про шаг 2. Замечаем интересную деталь — в латинской букве "K" читается и "T". Думаем о монограмме.</li>
-						<li><b>Шаг 5.</b> Понимаем, что читается также и "IT", что делает деталь еще интереснее</li>
-						<li><b>Шаг 6.</b> Вспоминаем о шаге 2. Гневаемся</li>
-						<li><b>Вывод:</b> Оставляем идею с монограммой как потенциально удачную на будущее. Ищем образ универсальнее</li>
-					</ul>
-				</div>
-			</div>
-			<div class="process-block">
-				<h3 class="process-block__title">
-					Торг 
-				</h3>
-				<div class="process-block__content">
-					<img
-						src="../img/trade.png"
-						alt="Торг"
-						class="process-block__img"
-					/>
-					<ul class="process-block__list process-block__list--trade">
-						<li><b>Шаг 1.</b> За бог знает какие деньги и услуги просим знакомых нарисовать по одному логотипу</li>
-						<li><b>Шаг 2.</b> Снова получаем свастики</li>
-						<li><b>Шаг 3.</b> Уверенно идем в стадию 4</li>
-						<li><b>Вывод:</b> Крепко задумываемся об идеологических убеждениях друзей. Возвращаемся к ноше графического дизайнера в одиночку</li>
-					</ul>
-				</div>
-			</div>
-			<div class="process-block">
-				<h3 class="process-block__title">
-					Депрессия 
-				</h3>
-				<div class="process-block__content">
-					<img
-						src="../img/depression.jpg"
-						alt="Депрессия "
-						class="process-block__img"
-					/>
-					<ul class="process-block__list process-block__list--depression">
-						<li><b>Шаг 1.</b> Включаем плейлист саундтреков к Max Payne 2, грустно смотрим вдаль</li>
-						<li><b>Вывод:</b> Хорошая игра</li>
-					</ul>
-				</div>
-			</div>
-			<div class="process-block">
-				<h3 class="process-block__title">
-					Принятие
-				</h3>
-			</div>
+			<swiper
+				ref="mySwiper"
+				:options="swiperOptions"
+			>
+				<swiper-slide class="process-block">
+					<h3 class="process-block__title">
+						Отрицание
+					</h3>
+					<div class="process-block__content">
+						<img
+							src="../img/denial.jpeg"
+							alt="Отрицание"
+							class="process-block__img"
+						/>
+						<ul class="process-block__list">
+							<li><b>Шаг 1.</b>  Обсуждаем первичные ассоциации с клевером, технологиями</li>
+							<li><b>Шаг 2.</b> Получаем логотипы перекрестка, Sims, Windows и свастики различных оттенков зеленого</li>
+							<li><b>Шаг 3.</b> Изучаем логотипы IT-компаний, логотипы с клевером</li>
+							<li><b>Шаг 4.</b> Понимаем, что все, что мы сделали, было уже сделано до нас. Печально</li>
+							<li><b>Вывод:</b> Осознанно отказываемся от классических образов клевера, прямых ассоциаций с технологиями и зеленого цвета как доминирующего</li>
+						</ul>
+					</div>
+				</swiper-slide>
+				<swiper-slide class="process-block">
+					<h3 class="process-block__title">
+						Гнев
+					</h3>
+					<div class="process-block__content">
+						<img
+							src="../img/anger.png"
+							alt="Гнев"
+							class="process-block__img process-block__img--anger"
+						/>
+						<ul class="process-block__list">
+							<li><b>Шаг 1.</b> Пытаемся спрятать образ клевер изящнее, обыграть технологии</li>
+							<li><b>Шаг 2.</b> Понимаем, что название компании может изменится</li>
+							<li><b>Шаг 3.</b> Гневаемся (при должном умении можно тихохонько ругнуться)</li>
+							<li><b>Шаг 4.</b> Забываем про шаг 2. Замечаем интересную деталь — в латинской букве "K" читается и "T". Думаем о монограмме.</li>
+							<li><b>Шаг 5.</b> Понимаем, что читается также и "IT", что делает деталь еще интереснее</li>
+							<li><b>Шаг 6.</b> Вспоминаем о шаге 2. Гневаемся</li>
+							<li><b>Вывод:</b> Оставляем идею с монограммой как потенциально удачную на будущее. Ищем образ универсальнее</li>
+						</ul>
+					</div>
+				</swiper-slide>
+				<swiper-slide class="process-block">
+					<h3 class="process-block__title">
+						Торг 
+					</h3>
+					<div class="process-block__content">
+						<img
+							src="../img/trade.png"
+							alt="Торг"
+							class="process-block__img"
+						/>
+						<ul class="process-block__list process-block__list--trade">
+							<li><b>Шаг 1.</b> За бог знает какие деньги и услуги просим знакомых нарисовать по одному логотипу</li>
+							<li><b>Шаг 2.</b> Снова получаем свастики</li>
+							<li><b>Шаг 3.</b> Уверенно идем в стадию 4</li>
+							<li><b>Вывод:</b> Крепко задумываемся об идеологических убеждениях друзей. Возвращаемся к ноше графического дизайнера в одиночку</li>
+						</ul>
+					</div>
+				</swiper-slide>
+				<swiper-slide class="process-block">
+					<h3 class="process-block__title">
+						Депрессия 
+					</h3>
+					<div class="process-block__content">
+						<img
+							src="../img/depression.jpg"
+							alt="Депрессия"
+							class="process-block__img"
+						/>
+						<ul class="process-block__list process-block__list--depression">
+							<li><b>Шаг 1.</b> Включаем плейлист саундтреков к Max Payne 2, грустно смотрим вдаль</li>
+							<li><b>Вывод:</b> Хорошая игра</li>
+						</ul>
+					</div>
+				</swiper-slide>
+				<swiper-slide class="process-block">
+					<h3 class="process-block__title">
+						Принятие
+					</h3>
+					<div class="process-block__content">
+						<img
+							src="../img/accept.jpg"
+							alt="Принятие"
+							class="process-block__img"
+						/>
+						<ul class="process-block__list">
+							<li><b>Шаг 1.</b> Включаем плейлист саундтреков к Max Payne 2, грустно смотрим вдаль</li>
+							<li><b>Вывод:</b> Хорошая игра</li>
+						</ul>
+					</div>
+				</swiper-slide>
+			</swiper>
 		</div>
 		<page-footer class="footer" />
 	</div>
@@ -104,15 +120,22 @@
 import { Vue, Component } from 'vue-property-decorator';
 import PageHeader from '@/components/PageHeader.vue';
 import PageFooter from '@/components/PageFooter';
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
 
 @Component({
 	components: { 
 		PageHeader,
 		PageFooter,
+		Swiper,
+    	SwiperSlide,
 	},
+	directives: {
+    	swiper: directive
+	}
 })
 
-export default class Process extends Vue {}
+export default class Process extends Vue {
+}
 </script>
 
 <style scoped lang="scss">
@@ -166,6 +189,8 @@ export default class Process extends Vue {}
 	}
 
 	&-block {
+		margin-bottom: 100px;
+		
 		&__title {
 			display: inline-block;
 			font-size: 24px;
