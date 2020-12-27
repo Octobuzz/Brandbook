467adcbf9d0e073f0886994052d23d181f5f4fa1
Ольга Пастухова <olg.pastukhova1@x5.ru>
2020-12-27 05:46:37 +0300

    Добавлена заглушка на ширину меньше 1100 пк

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
 	<div id="app">
-		<router-view />
+		<router-view
+			v-if="showContent"
+			class="content"
+		/>
+		<div
+			v-if="!showContent"
+			class="modal"
+		>
+			<p class="modal-headline">
+				Caution!
+			</p>
+			<p class="modal-text">
+				Всю мою красоту можно разглядеть только на экране шириной 1100px и более <br />
+				Спасибо за понимание <br />
+				И да, времени на мобильную версию не хватило
+			</p>
+		</div>
 	</div>
 </template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+
+export default class App extends Vue {
+  showContent = false;
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,6 +41,24 @@ body {
   margin: 0;
   font-size: 18px;
   line-height: 1.7;
+}
+
+.modal {
+  font-size: 16px;
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &-headline {
+    padding: 10px 25px;
+    font-size: 24px;
+    border: 2px solid $main;
+  }
+
+  &-text {
+    margin: 0;
+  }
 }
 
 </style>
